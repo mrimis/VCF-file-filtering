b54e6b77fe08e6a4aaa18e26e9561de0f2cdc6d9
Mrinal Mishra <mrinalmishra@ufl.edu>
2020-09-19 00:49:00 -0400

    Update SNP_in_window_pos.py

--- SNP_in_window_pos.py
+++ SNP_in_window_pos.py
@@ -1,10 +1,10 @@
 
 fdata = open("Final_filtered_All_60_SNPS.vcf", "rU") # Open vcf file
-SCS=[1,22,23,24,25,26,27,28,29,30]#Indexes of specimen belonging to SCS allopatric population
+SCS=[1,22,23,24,25,26,27,28,29,30]#Indexes of samples belonging to SCS allopatric population
 SCS_Allo = [x-1 for x in SCS]
-ECS=[3,5,6,12,13,14,16,17,19,20,37,38,40,41,42,43,54,55,56,57,58,59,60]#Indexes of specimen belonging to ECS population
+ECS=[3,5,6,12,13,14,16,17,19,20,37,38,40,41,42,43,54,55,56,57,58,59,60]#Indexes of samples belonging to ECS population
 ECS_Symp = [x-1 for x in ECS]
-SCS_Sym = [2,4,7,8,9,10,11,15,18,21,31,32,33,34,35,36,44,45,46,47,48,49,50,51,52,53]#Indexes of specimen belonging to SCS Sympatric population
+SCS_Sym = [2,4,7,8,9,10,11,15,18,21,31,32,33,34,35,36,44,45,46,47,48,49,50,51,52,53]#Indexes of samples belonging to SCS Sympatric population
 SCS_Symp = [x-1 for x in SCS_Sym]
 list1=[]
 with open("SCS_Symp_scaffold8_7509_7927.txt", "w") as fp, open("SCS_Allo_scaffold8_7509_7927.txt","w") as fp1,  open("ECS_scaffold8_7509_7927.txt","w") as fp2: #Open file for writing
